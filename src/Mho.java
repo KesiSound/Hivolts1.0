61c762eb2bceb11b61d55f38082ab084ecc37315
KesiSound <kswkks2719@gmail.com>
2016-09-08 11:20:31 -0700

    Added framework for AI

--- src/Mho.java
+++ src/Mho.java
@@ -1,4 +1,21 @@
 
 public class Mho {
-
+	//Add constructors later
+	
+	
+	//AI Part
+	
+	void AI() { 
+		//Vertical/horizontal, move one square towards
+		
+		//Diagonally towards
+		
+		//If horizontal distances >= vertical distance, move horizontally
+		
+		//If vertical distance >= horizontal distance, move vertically
+		
+		//Move into electric fence
+		
+		//Do not move
+	}
 }
