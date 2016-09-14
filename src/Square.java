c75bfe59833a4020ab3bec6e65ae33e7a184afec
AashaiAvadhani <aashai123@gmail.com>
2016-09-14 10:51:24 -0700

    Square Class changes
    Edited the square class by adding a constructor, some getters for each
    instance variable

--- src/Square.java
+++ src/Square.java
@@ -1,4 +1,27 @@
-
-public class Square {
-
+import javax.swing.JComponent;
+public class Square extends JComponent {
+	private int x;
+	private int y;
+	private char filled;
+	
+	
+	public Square(){
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public char getFilled(){
+		return filled;
+	}
+	
+	
 }
