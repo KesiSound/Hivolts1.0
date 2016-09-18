728cff16585c3db9f258795a1f1dfc371b634da4
KesiSound <kswkks2719@gmail.com>
2016-09-18 11:16:33 -0700

    added status characteristic for square

--- src/Square.java
+++ src/Square.java
@@ -2,12 +2,13 @@ import javax.swing.JComponent;
 public class Square extends JComponent {
 	private int x;
 	private int y;
-	private char filled;
+	private char status;
 	
 	
-	public Square(int x, int y){
+	public Square(int x, int y, char status){
 		this.x = x;
 		this.y = y;
+		this.status = status; //status will hold a character like 'f' - fence, 'm' - mho, 'p' - player, or 'u' -unfilled
 	}
 	
 	
@@ -19,8 +20,8 @@ public class Square extends JComponent {
 		return y;
 	}
 	
-	public char getFilled(){
-		return filled;
+	public char getStatus(){
+		return status;
 	}
 	
 	
